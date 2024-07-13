93216d7671c914e8f0cc7ca1c669fda1d88948a1
Albert Robin Moriarty <19686802+armoriarty@users.noreply.github.com>
2024-07-13 18:48:17 -0400

    Display the Incoming Get Request

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,6 +9,17 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection Established!")
+        handle_connection(stream)
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|each_result| each_result.unwrap())
+        .take_while(|each_line| !each_line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request)
 }
