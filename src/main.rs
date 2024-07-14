95f87c0c413816b5fb32f4a68ecd90be3afc260d
Albert Robin Moriarty <19686802+armoriarty@users.noreply.github.com>
2024-07-14 15:12:44 -0400

    Add Multiple Responses Based On Request
    - Add .gitignore to clean up

--- src/main.rs
+++ src/main.rs
@@ -16,22 +16,23 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|each_result| each_result.unwrap())
-        .take_while(|each_line| !each_line.is_empty())
-        .collect();
 
-    println!("Request: {:#?}", http_request);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let file_path = format!("./static/html/{filename}");
+    let contents = fs::read_to_string(file_path).unwrap();
     let length = contents.len();
 
     let response = format!(
         "{status_line}\r\n\
-         Content-Length: {length}\r\n\r\n\
-         {contents}"
+        Content-Length: {length}\r\n\r\n\
+        {contents}"
     );
 
     println!("Response: {:#?}", response);
